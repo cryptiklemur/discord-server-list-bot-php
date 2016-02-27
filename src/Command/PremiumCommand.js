ce16cb2a25070be88a2ed47c5fa1ed39db9883fc
Aaron Scherer <aequasi@gmail.com>
2016-02-26 22:49:22 -0800

    Adding premium stuff

--- src/Command/PremiumCommand.js
+++ src/Command/PremiumCommand.js
@@ -1,4 +1,5 @@
 const AbstractCommand = require('discord-bot-base').AbstractCommand;
+const Server          = require('../Model/Server');
 
 class PremiumCommand extends AbstractCommand {
     static get name() {
@@ -15,12 +16,24 @@ class PremiumCommand extends AbstractCommand {
                 return;
             }
 
-            let host    = this.container.getParameter('env') === 'dev' ? 'dev' : 'www',
-                message = `Premium Servers are servers servers that bid to be shown higher on the server listing,
+            Server.findOne({identifier: this.message.server.id}, (error, server) => {
+                if (error) {
+                    return this.logger.error(error);
+                }
+
+                if (!server) {
+                    return this.reply("Couldn't find your server. Strange.");
+                }
+
+                let host    = this.container.getParameter('env') === 'dev' ? 'dev' : 'www',
+                    message = `Premium Servers are servers servers that bid to be shown higher on the server listing,
 allowing the server to get more traffic. Bidding starts ar $5 a month, and has no upper limit.
 
-To place a bid for premium, visit the DiscordServers.com site here: http://${host}.discordservers.com/premium/${this.message.server.id}
+To place a bid for premium, visit the DiscordServers.com site here: http://${host}.discordservers.com/premium/${server.id}
 `;
+
+                this.reply(message);
+            });
         });
     }
 }
